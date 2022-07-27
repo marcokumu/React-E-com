3a840f81caa4c19aa1c577f6c7061f3ff5cfb45b
Okumu Mark <marcokumu10@gmail.com>
2022-07-28 00:37:44 +0300

    updated the carousel section

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,3 +20,19 @@ const Home = () => {
                             <img src="banners/4.png" className="d-block w-100" alt="..." />
                         </div>
                     </div>
+                    <button className="carousel-control-prev" type="button" data-bs-target="#FakeShopCarouselInterval" data-bs-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Previous</span>
+                    </button>
+                    <button className="carousel-control-next" type="button" data-bs-target="#FakeShopCarouselInterval" data-bs-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Next</span>
+                    </button>
+                </div>
+                <Products />
+            </div>
+        </>
+    )
+}
+
+export default Home;
