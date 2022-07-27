e18449303be757ade0dacbe40d7001184e39dd51
Okumu Mark <marcokumu10@gmail.com>
2022-07-28 00:49:31 +0300

    further updates

--- src/components/Products.js
+++ src/components/Products.js
@@ -25,7 +25,7 @@ function Products() {
         }
         getProdcuts();
     }, []);
-    
+
     const Loading = () => {
         return (
             <>
@@ -40,3 +40,35 @@ function Products() {
                     </div>
 
                 </div>
+
+                <div className="col-md-9 py-md-3">
+                    <div className="row">
+                        <div className="col-6 col-md-6 col-lg-4 mb-3">
+                            <Skeleton height={400} width={"100%"}/>
+                        </div>
+                        <div className="col-6 col-md-6 col-lg-4 mb-3">
+                            <Skeleton height={400} width={"100%"}/>
+                        </div>
+                        <div className="col-6 col-md-6 col-lg-4 mb-3">
+                            <Skeleton height={400} width={"100%"}/>
+                        </div>
+                        <div className="col-6 col-md-6 col-lg-4 mb-3">
+                            <Skeleton height={400} width={"100%"}/>
+                        </div>
+                        <div className="col-6 col-md-6 col-lg-4 mb-3">
+                            <Skeleton height={400} width={"100%"}/>
+                        </div>
+                        <div className="col-6 col-md-6 col-lg-4 mb-3">
+                            <Skeleton height={400} width={"100%"}/>
+                        </div>
+                        <div className="col-6 col-md-6 col-lg-4 mb-3">
+                            <Skeleton height={400} width={"100%"}/>
+                        </div>  
+                    </div>
+
+                </div>
+
+
+            </>
+        )
+    }
