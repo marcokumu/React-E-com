45735b22ebc75cfdabe94943ef5585bf61305de1
Okumu Mark <marcokumu10@gmail.com>
2022-07-28 00:50:25 +0300

    second part of the filter process

--- src/components/Products.js
+++ src/components/Products.js
@@ -72,3 +72,22 @@ function Products() {
             </>
         )
     }
+    const filterProduct = (category) => {
+        const updateList = data.filter((x) => x.category === category);
+        setFilter(updateList);
+    }
+
+    const ShowProducts = () => {
+        return (
+            <>
+                <div className="col-md-3 my-3">
+
+                    <div className="position-sticky" style={{ top: "100px" }}>
+                        <button className="btn btn-outline-dark m-1 btn-sm" onClick={() => setFilter(data)}>All</button>
+                        <button className="btn btn-outline-dark m-1 btn-sm" onClick={() => filterProduct("women's clothing")}>Women's Clothing</button>
+                        <button className="btn btn-outline-dark m-1 btn-sm" onClick={() => filterProduct("men's clothing")}>Men's Clothing</button>
+                        <button className="btn btn-outline-dark m-1 btn-sm" onClick={() => filterProduct("jewelery")}>Jewelery</button>
+                        <button className="btn btn-outline-dark m-1 btn-sm" onClick={() => filterProduct("electronics")}>Electronics</button>
+                    </div>
+
+                </div>
