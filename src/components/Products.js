182cf3d27863277a70c0192861098f39148c1dc0
Okumu Mark <marcokumu10@gmail.com>
2022-07-28 00:52:37 +0300

    final touches on products component

--- src/components/Products.js
+++ src/components/Products.js
@@ -12,7 +12,7 @@ function Products() {
         let componentMounted = true;
         const getProdcuts = async () => {
             setLoading(true);
-            const response = await fetch('https://fakestoreapi.com/products');
+            const response = await fetch('https://electronics-details.herokuapp.com/electronics');
             if (componentMounted) {
                 const data = await response.json();
                 setData(data);
@@ -91,3 +91,48 @@ function Products() {
                     </div>
 
                 </div>
+
+                <div className="col-md-9 py-md-3">
+                    <div className="row">
+                        {filter.map((product) => {
+                            return (
+                                <div className="col-6 col-md-6 col-lg-4 mb-3" key={product.id}>
+
+                                    <div className="card h-100">
+                                        <img src={product.image} className="m-3" style={{ height: "300px", width: "auto", objectFit: "contain" }} alt={product.title} />
+                                        <div className="m-3 mb-0">
+                                            <small className="card-title">{product.title.substring(0, 50)}...</small>
+                                        </div>
+                                        <div style={{ marginTop: "auto" }}>
+                                            <div className="d-flex justify-content-between align-items-center">
+                                                <div className="m-3"><b>${product.price}</b></div>
+                                                <NavLink className="stretched-link" to={`/product/${product.id}`}>
+                                                    <button className="btn btn-sm m-3 border-primary">
+                                                        <i className="fa fa-arrow-right text-muted"></i>
+                                                    </button>
+                                                </NavLink>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+
+                </div>
+
+
+            </>
+        )
+    }
+
+    return (
+        <div className="container">
+            <div className="row">
+                {loading ? <Loading /> : <ShowProducts />}
+            </div>
+        </div>
+    )
+}
+
+export default Products
