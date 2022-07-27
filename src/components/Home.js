2a77aa1e4b2d8af1495d9acdf756c0fb80e5ca3f
Okumu Mark <marcokumu10@gmail.com>
2022-07-28 00:36:27 +0300

    added section a carousel

---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Products from './Products';
+
+const Home = () => {
+    return (
+        <>
+            <div className="container px-0" style={{ marginTop: "66px" }}>
+                <div id="FakeShopCarouselInterval" className="carousel slide" data-bs-ride="carousel">
+                    <div className="carousel-inner">
+                        <div className="carousel-item active" data-bs-interval="10000">
+                            <img src="banners/1.png" className="d-block w-100" alt="..." />
+                        </div>
+                        <div className="carousel-item" data-bs-interval="2000">
+                            <img src="banners/2.png" className="d-block w-100" alt="..." />
+                        </div>
+                        <div className="carousel-item" data-bs-interval="2000">
+                            <img src="banners/3.png" className="d-block w-100" alt="..." />
+                        </div>
+                        <div className="carousel-item" data-bs-interval="2000">
+                            <img src="banners/4.png" className="d-block w-100" alt="..." />
+                        </div>
+                    </div>
