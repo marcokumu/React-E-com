51df3a6c4e51eb3b0d9a47f89f9946d5b6d980f0
Okumu Mark <marcokumu10@gmail.com>
2022-07-28 00:48:33 +0300

    creating filters for products

--- src/components/Products.js
+++ src/components/Products.js
@@ -25,3 +25,18 @@ function Products() {
         }
         getProdcuts();
     }, []);
+    
+    const Loading = () => {
+        return (
+            <>
+                <div className="col-md-3 my-3">
+
+                    <div className="position-sticky" style={{ top: "100px" }}>
+                        <button className="btn btn-outline-dark m-1 btn-sm" onClick={() => setFilter(data)}>All</button>
+                        <button className="btn btn-outline-dark m-1 btn-sm" onClick={() => filterProduct("women's clothing")}>Women's Clothing</button>
+                        <button className="btn btn-outline-dark m-1 btn-sm" onClick={() => filterProduct("men's clothing")}>Men's Clothing</button>
+                        <button className="btn btn-outline-dark m-1 btn-sm" onClick={() => filterProduct("jewelery")}>Jewelery</button>
+                        <button className="btn btn-outline-dark m-1 btn-sm" onClick={() => filterProduct("electronics")}>Electronics</button>
+                    </div>
+
+                </div>
